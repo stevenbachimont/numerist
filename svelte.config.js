a9227b16133901cafda2599ad85ccc7fde6459e1
stevenbachimont <stevenbachimont@gmail.com>
2025-02-09 08:07:06 +0100

    go back

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,7 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-node'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter()
-  }
 }
-
-export default config
